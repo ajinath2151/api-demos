3a71d7a15669a23f09395d267076c3f04701df4c
ajinath <ajinath2151@gmail.com>
2023-04-04 00:08:35 +0530

    paginaiton working properly..

--- src/pages/FakeApi03.js
+++ src/pages/FakeApi03.js
@@ -1,68 +1,61 @@
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
 function FakeApi03() {
-  const [data, setData] = useState([]);
+  const [items, setItems] = useState([]);
 
   const colorObject = {
-    titileColor:{
-        color:"darkOrange",
+    titileColor: {
+      color: "darkOrange",
     },
-    emailColor:{
-      color:"green"
+    emailColor: {
+      color: "green",
     },
-    bodyColor:{
-      color:'MediumVioletRed',
-    }
-  }
-  // useEffect(() => {     
+    bodyColor: {
+      color: "MediumVioletRed",
+    },
+  };
+  // useEffect(() => {
   //   fetch("https://jsonplaceholder.typicode.com/comments?_page=1&_limit=20").then((result) => {
   //     result.json().then((response) => {
   //       setData(response);
   //     });
   //   });
   // }, []);
-  useEffect(() => {     
-    fetch("https://jsonplaceholder.typicode.com/comments?_page=1&_limit=20").then((result) => {
-      result.json().then((response) => {
-        setData(response);
-      });
-    });
+  useEffect(() => {
+    const getComments = async () => {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/comments?_page=1&_limit=20"
+      );
+      const data = await res.json();
+      setItems(data);
+    };
+    getComments();
   }, []);
-  console.log(data);
+  console.log(items);
 
-  const handlePageClick=((data)=> {
+
+  const fetchComments = async (currentPage)=>{
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=20`
+    );
+    const data = await res.json();
+    return data;
+  }
+
+  const handlePageClick = async (data) => {
     // console.log("clicked...")
     // console.log(data) show selected page number
-    console.log(data.selected) //give us page number as integer value
-  })
+    console.log(data.selected); //give us page number as integer value
+    let currentPage=data.selected+1;
+    const commentFromServer = await fetchComments(currentPage);
+    setItems(commentFromServer);
+  };
   return (
     <>
 
-    {/* next and previous is default value of pagination and we can add some ohher attribute in ReactPagination as below*/}
-    <ReactPaginate 
-        previousLabel={"Previous"} //default value
-        nextLabel={"Next"} //default value
-        breakLabel={"..."} //default value
-        pageCount={20} //show total number of pages (suggested dynamic)
-        marginPagesDisplayed={2} // how many pages at start and end will be show
-        pageRangeDisplayed={3} // on click of breakLabel i.e. (...) three dots show hidden pages of break label 
-        onPageChange={handlePageClick} // call this mehtod when user click on page
-        containerClassName={'pagination justify-content-center'} // css class for unordered list
-        pageClassName={'page-item'} //classname for each pages
-        pageLinkClassName={'page-link'} //classname of a href tag
-        previousClassName={'page-item'} // add same style for previous button as other because use same bootstrap class
-        previousLinkClassName={'page-link'} //add same style for previous className for a href tag because use same class name as other
-        nextClassName={'page-item'} // add same style for next button as other because use same bootstrap class
-        nextLinkClassName={'page-link'} //add same style for next className for a href tag because use same class name as other
-        breakClassName={'page-item'} // add same style for breakLabel button as other because use same bootstrap class
-        breakLinkClassName={'page-link'} //add same style for breakLabel className for a href tag because use same class name as other
-        activeClassName={"active"} // to show which page is currently is displayed        
-      />
-
-
-      {/* <div className="container">
+    <div className="container">
         <div className="row">
-          {data.map((item,index) => (
+          {items.map((item,index) => (
             <div className="col col-md-3" key={index}>
               <div className="card">
                 <div className="card-body">
@@ -83,7 +76,31 @@ function FakeApi03() {
             </div>
           ))}
         </div>
-      </div> */}
+      </div>
+
+
+      {/* next and previous is default value of pagination and we can add some ohher attribute in ReactPagination as below*/}
+      <ReactPaginate
+        previousLabel={"Previous"} //default value
+        nextLabel={"Next"} //default value
+        breakLabel={"..."} //default value
+        pageCount={20} //show total number of pages (suggested dynamic)
+        marginPagesDisplayed={2} // how many pages at start and end will be show
+        pageRangeDisplayed={3} // on click of breakLabel i.e. (...) three dots show hidden pages of break label
+        onPageChange={handlePageClick} // call this mehtod when user click on page
+        containerClassName={"pagination justify-content-center"} // css class for unordered list
+        pageClassName={"page-item"} //classname for each pages
+        pageLinkClassName={"page-link"} //classname of a href tag
+        previousClassName={"page-item"} // add same style for previous button as other because use same bootstrap class
+        previousLinkClassName={"page-link"} //add same style for previous className for a href tag because use same class name as other
+        nextClassName={"page-item"} // add same style for next button as other because use same bootstrap class
+        nextLinkClassName={"page-link"} //add same style for next className for a href tag because use same class name as other
+        breakClassName={"page-item"} // add same style for breakLabel button as other because use same bootstrap class
+        breakLinkClassName={"page-link"} //add same style for breakLabel className for a href tag because use same class name as other
+        activeClassName={"active"} // to show which page is currently is displayed
+      />
+
+      
     </>
   );
 }
